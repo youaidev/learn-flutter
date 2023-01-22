39bc5cf194a39558725b8a7d1ebb8894ec3cf708
Youcef AIDOUN <108759297+youaidev@users.noreply.github.com>
2023-01-22 05:01:42 +0100

    Update main.dart

--- n1_hello_world/lib/main.dart
+++ n1_hello_world/lib/main.dart
@@ -1,8 +1,30 @@
+// packages
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Center(child: Text('hello world')),
-  ));
+  runApp(
+    HelloWorldApp(),
+  ); // runApp
+} // main
+
+class HelloWorldApp extends StatelessWidget {
+  const HelloWorldApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+        ),
+        appBar: AppBar(
+          title: Text('appbar'),
+        ),
+        body: Center(
+          child: Text('hello world'),
+        ),
+      ),
+    );
+  }
 }
