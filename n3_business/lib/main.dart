73a7b7808bbe1b9ba10343eb3d103aaf3da5d409
Youcef AIDOUN <108759297+youaidev@users.noreply.github.com>
2023-01-29 08:09:30 +0100

    Update main.dart

--- n3_business/lib/main.dart
+++ n3_business/lib/main.dart
@@ -12,33 +12,89 @@ class BusinessCard extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Color(0XFFF4F4F4),
-        body: Center(
-          child: Column(
-            children: [
-              CircleAvatar(
-                radius: 120,
-                child: Container(
-                  width: 230.0,
-                  height: 230.0,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    image: DecorationImage(
-                      fit: BoxFit.fill,
-                      image: AssetImage('images/businesscard.jpg'),
-                    ),
+        body: Column(
+          children: [
+            CircleAvatar(
+              radius: 120,
+              child: Container(
+                width: 230.0,
+                height: 230.0,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    fit: BoxFit.fill,
+                    image: AssetImage('images/businesscard.jpg'),
                   ),
                 ),
               ),
-              Text(
-                'Youcef Aidoun',
-                style: TextStyle(
-                  color: Colors.blue[800],
-                  fontSize: 45,
-                  fontFamily: 'Allura',
+            ),
+            Text(
+              'Youcef Aidoun',
+              style: TextStyle(
+                color: Colors.blue[800],
+                fontSize: 45,
+                fontFamily: 'Allura',
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(16),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.white,
                 ),
-              )
-            ],
-          ),
+                height: 70,
+                child: Row(
+                  children: [
+                    Spacer(flex: 1),
+                    Icon(
+                      Icons.phone,
+                      size: 40,
+                      color: Colors.blue,
+                    ),
+                    Spacer(flex: 1),
+                    Text(
+                      ' +213 78 76 65 53',
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontFamily: 'Allura',
+                      ),
+                    ),
+                    Spacer(flex: 2),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(16),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.white,
+                ),
+                height: 70,
+                child: Row(
+                  children: [
+                    Spacer(flex: 1),
+                    Icon(
+                      Icons.email,
+                      size: 40,
+                      color: Colors.blue,
+                    ),
+                    Spacer(flex: 1),
+                    Text(
+                      ' youcefaidoun@email.com',
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontFamily: 'Allura',
+                      ),
+                    ),
+                    Spacer(flex: 1),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
